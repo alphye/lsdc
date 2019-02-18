1220eebcb5983eda02da5ca7d7253928a76886f9
ls-alphye <10103765@qq.com>
2019-02-18 11:04:01 +0800

    引入dubbo框架

---
+++ lsdc-common/src/main/java/com/lishengzn/common/service/VehicleService.java
@@ -0,0 +1,8 @@
+package com.lishengzn.common.service;
+
+public interface VehicleService {
+    void vehicleOnline(String ip);
+    void vehicleOffline(String ip);
+    void allVehiclesOnline();
+    void allVehiclesOffline();
+}
